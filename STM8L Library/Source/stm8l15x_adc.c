794b928783de3f0daedbc801e8403243310d8911
sorooshtehrany <soroosh.tehrany@gmail.com>
2023-05-05 00:52:36 +0430

    update adc

--- STM8L Library/Source/stm8l15x_adc.c
+++ STM8L Library/Source/stm8l15x_adc.c
@@ -1,4 +1,4 @@
-/**
+/****
   ******************************************************************************
   * @file    stm8l15x_adc.c
   * @author  MCD Application Team
@@ -789,7 +789,7 @@ void ADC_DMACmd(ADC_TypeDef* ADCx, FunctionalState NewState)
   ---------- 
      1. ADC_FLAG_OVR : Overrun detection when ADC channel converted data is lost
 
-     2. ADC_FLAG_EOC : End of conversion - to indicate the end of a regular 
+     2. ADC_FLAG_EOC : End of conversionï¿½- to indicate the end of a regular 
                   CHANNEL conversion or a GROUP conversions, depending of the 
                   ADC Continuous Conversion Mode (Continuous or Single 
                   conversion) and of the DMA usage.
@@ -937,7 +937,7 @@ ITStatus ADC_GetITStatus(ADC_TypeDef* ADCx,
 }
 
 /**
-  * @brief  Clears the ADC’s interrupt pending bits.
+  * @brief  Clears the ADCï¿½s interrupt pending bits.
   * @param  ADCx where x can be 1 to select the specified ADC peripheral.
   * @param  ADC_IT: specifies the ADC interrupt pending bit to clear.
   *          This parameter can be one of the following values:
